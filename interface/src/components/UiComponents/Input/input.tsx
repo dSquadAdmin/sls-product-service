e69bb36739b816f38d43eef8448dd5f623359e46
dSquadAdmin <keshavbist19@gmail.com>
2023-04-26 23:03:48 +0545

    Validated InputField

--- interface/src/components/UiComponents/Input/input.tsx
+++ interface/src/components/UiComponents/Input/input.tsx
@@ -1,40 +1,39 @@
-import React from 'react'
+import React from "react";
 
 interface IProps {
-    name: string;
-    redacted: boolean;
-    label: string;
-    isError: boolean;
-    placeholder: string;
-    message: string;
-    onChange: (value:any)=>void;
+  name: string;
+  label: string;
+  isError: boolean;
+  placeholder: string;
+  message: string;
+  value: string|number|undefined;
+  type: "text"|"number"|"url"|"password";
+  onChange: (value: any) => void;
 }
 const InputField = (props: IProps) => {
-    const {
-        name,
-        redacted,
-        label,
-        isError,
-        placeholder,
-        message,
-        onChange
-    } = props;
-    const errClass = isError ? 'border-red-500' : '';
+  const { name, label, isError, placeholder, message, type, value, onChange } =
+    props;
+  const errClass = isError ? "border-red-500" : "";
 
-    return (
-        <div className="mb-6">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={name}>
-            {label}
-        </label>
-        <input 
-            className={`shadow appearance-none border ${errClass} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
-            id="password" type={redacted ? "password" : "text"} 
-            placeholder={placeholder} 
-            onChange={onChange}
-        />
-        {isError && <p className="text-red-500 text-xs italic">{message}</p>}
-        </div>
-    );
+  return (
+    <div className="mb-6">
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor={name}
+      >
+        {label}
+      </label>
+      <input
+        className={`shadow appearance-none border ${errClass} rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+        id="password"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      {isError && <p className="text-red-500 text-xs italic">{message}</p>}
+    </div>
+  );
 };
 
 export default InputField;
